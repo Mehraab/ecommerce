cba54e054e1b79eaa5a06a471dc741d3fad1e589
hasib-003 <gmmehrab@gmail.com>
2024-08-13 02:29:25 +0600

    updated the user model

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,14 +1,16 @@
 const express = require('express');
-const { register, login, getAllUsers, getaUser, deleteUser, updateUser } = require('../controller/userController');
+const { register, login, getAllUsers, getaUser, deleteUser, updateUser, blockUser, unblockUser } = require('../controller/userController');
 const { protect, admin } = require('../middlewares/auth');
 const router = express.Router();
 
 
 router.post('/register', register);
 router.post('/login', login);
-router.get('/getAllUsers',admin, getAllUsers);
-router.get('/:id',protect, getaUser);
-router.delete('/:id', protect, deleteUser);
-router.put('/:id',protect, updateUser);
+router.get('/getAllUsers',protect,admin, getAllUsers);
+router.get('/getauser',protect, getaUser);
+router.delete('/delete', protect, deleteUser);
+router.put('/edituser', protect, updateUser);
+router.put('/block/:id',protect, admin, blockUser);
+router.put('/unblock/:id',protect, admin, unblockUser);
 
 module.exports = router;
